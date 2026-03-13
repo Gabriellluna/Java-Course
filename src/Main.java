752b5fd3fbea1b6a33d0c75e108d0e1d1266efd7
Gabriel Luna <glunamaia@gmail.com>
2026-03-13 16:10:09 -0300

    explorando o funcionamento do printf e println

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println(("Hello World!"));
-        int steps = 500;
-        double height = 1.75;
-        boolean vf = true;
-
-        Paciente gabriel = new Paciente();
-        System.out.println((gabriel));
-
-        //conversão explícita é usada para casos que vão contra a hierarquia -> double > int
-        int alturaEmCm = (int) (height * 100);
-        System.out.println((alturaEmCm));
-
-        //conversão implícita só funciona na ordem da hierarquia -> int > double
-        int valorInt = 19;
-        double valorDouble = valorInt;
-        System.out.println(valorDouble);
+        float nota1 = 7.5f;
+        float nota2 = 8;
+        float nota3 = 9;
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        nota1 = scanner.nextFloat();*/
+        float media = (nota1+nota2+nota3) / 3;
+        System.out.printf("Sua média é %f \n", media); /*printf colocamos o %[tipo var] e depois da vírgula a var*/
+        System.out.println("Sua média é " + media); /*println colocamos a var no meio do texto com "" + ""*/
 
     }
 }
