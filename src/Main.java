fbec64485c0bd677da784a2bf019660a19656db3
Gabriel Luna <glunamaia@gmail.com>
2026-03-13 17:39:51 -0300

    uso de constantes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        double temp = 20;
-        double farenheit = (temp * 9/5) + 32;
-        System.out.printf("Temperatura Celsius: %.2f | Temperatura Farenheit: %.2f \n"
-        , temp, farenheit);
-        System.out.println("Temperatura Celsius: " + temp + "Temperatura Farenheit: " + farenheit);
+    public static void main(String[] args) {
+        /*criando constantes para o programa*/
+        final double CONSUMO_MEDIO = 12.5;
+        final double CAPACIDADE = 50;
+        final double COMBUSTIVEL = 20;
+        final double DISTANCIA = 280;
 
+        System.out.println("A capacidade máxima do veículo é de "+(int) (CONSUMO_MEDIO*CAPACIDADE)+" km com um tanque cheio");
+        System.out.printf("Autonomia atual: %.0f \n", CONSUMO_MEDIO*COMBUSTIVEL);
+        if (CONSUMO_MEDIO*COMBUSTIVEL < DISTANCIA){
+            System.out.println("Você não conseguirá finalizar a viagem");
+        }else{
+            System.out.println("Você conseguirá chegar ao seu destino");
+        }
     }
 }
