3908365d6090fd1a16e61002e2693bc7bc08fabd
Gabriel Luna <glunamaia@gmail.com>
2026-03-13 16:15:02 -0300

    explorando mais uma vez a diferença entre println e printf, programa de conversão de Celsius para Farenheit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        float nota1 = 7.5f;
-        float nota2 = 8;
-        float nota3 = 9;
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        nota1 = scanner.nextFloat();*/
-        float media = (nota1+nota2+nota3) / 3;
-        System.out.printf("Sua média é %f \n", media); /*printf colocamos o %[tipo var] e depois da vírgula a var*/
-        System.out.println("Sua média é " + media); /*println colocamos a var no meio do texto com "" + ""*/
+        double temp = 20;
+        double farenheit = (temp * 9/5) + 32;
+        System.out.printf("Temperatura Celsius: %.2f | Temperatura Farenheit: %.2f \n"
+        , temp, farenheit);
+        System.out.println("Temperatura Celsius: " + temp + "Temperatura Farenheit: " + farenheit);
 
     }
 }
