6810d73c2099c1ae12637484d668cf30a302a356
Gabriel Luna <glunamaia@gmail.com>
2026-04-02 21:59:26 -0300

    regex parte1

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,46 @@
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Main
 {
+    public static String formatarTelefone(String telefone){
+        Pattern pattern = Pattern.compile("(\\d{2})(\\d{4,5})(\\d{4})");
+        Matcher match = pattern.matcher(telefone);
+        if(match.find()){
+            return String.format("Número de telefone com máscara: (%s) %s-%s", match.group(1), match.group(2), match.group(3));
+        }else{
+            return "Número de telefone inválido";
+        }
+    }
+    public static String verificaEmail(String email){
+        Pattern pattern1 = Pattern.compile("[a-zA-Z0-9_.-]+@[a-zA-Z0-9]+\\.[a-z]{2}");
+        Pattern pattern2 = Pattern.compile("[a-zA-Z0-9_.-]+@[a-zA-Z0-9]+\\.[a-z]{2}\\.[a-z]{2}");
+
+        Matcher match1 = pattern1.matcher(email);
+        Matcher match2 = pattern2.matcher(email);
+
+        if(match2.find()){
+            return "O seu e-mail é válido pelo match2";
+        } else if(match1.find()){
+            return "O seu e-mail é válidasso";
+        } else {
+            return "O seu e-mail é inválido";
+        }
+    }
+
     public static void main(String[] args) {
-        //string é uma classe em Java
-        String nome = "Gabriel";
-        String nome_composto = """
-                Gabriel
-                18 anos
-                Dev Jr
-                """;
+        //Regex - Expressões regulares - Regular Expressions
+        String texto = "Meu e-mail é #gabriel@gmail.com.brbrbr";
+        //como eu verifico se dentro do texto há um padrão de e-mail?
+        //COM REGEX! api REGEZ
+        //DUAS CLASSES: PATTERN - onde vamos definir o padrão
+        //              MATCHER - verificação de match
+        System.out.println(formatarTelefone("11548951"));
+        System.out.println(verificaEmail("gabriellunamaia@gmail.com.br"));
 
-        //formatação de String
-        String nome_grande = String.format("Nome: %s \nNome Composto: %s", nome, nome_composto);
-        System.out.println(nome_grande);
-        //para formatar:
-        // %s - String
-        // %d - int
-        // %f - float/double
-        // %.2f - formata com limite de casas decimais
 
-        //métodos de manipulação
-        // trim() - tira o espaço em branco da String
-        // toLowerCase()/toUpperCase()
-        // replace()
-        // chartAt() - pega um caractere específico de uma String
-        // substring() - pega uma faixa de caracteres
-        // ...
-        System.out.println(nome_grande.toUpperCase());
-        System.out.println(nome_grande.replace("18 anos", "20 anos"));
-        System.out.println(nome_grande.charAt(0));
-        System.out.println(nome_grande.substring(0,5));
+
+
     }
 }
