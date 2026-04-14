f383dbd4f741ed377f0c038446362ae1104f8f7c
Gabriel Luna <glunamaia@gmail.com>
2026-04-14 19:54:20 -0300

    verifica CPF

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,17 @@ public class Main
 {
      public static void main(String[] args) {
          Scanner scan = new Scanner(System.in);
-         System.out.println("Insira um código");
+         System.out.println("Insira o CPF");
          String code = scan.nextLine();
 
-         Pattern pattern = Pattern.compile("^[A-Z]{3}-[0-9]{4}");
+         Pattern pattern = Pattern.compile("^[0-9]{3}-[0-9]{3}-[0-9]{3}.[0-9]{2}");
          Matcher matcher = pattern.matcher(code);
-         if(matcher.find()){
-             System.out.printf("Código correto no matcher 1 : %s\n", matcher.group());
-         }else{
-             System.out.println("Código inválido no matcher 1!");
-         }
-         //agora vamos ver sem usar o ^
 
-         Pattern pattern2 = Pattern.compile("[A-Z]{3}-[0-9A-Za-z]{4}");
-         Matcher matcher2 = pattern2.matcher(code);
-         if(matcher2.matches()){
-             System.out.printf("Código correto no matcher 2: %s\n", matcher2.group());
+
+         if (matcher.matches()){
+             System.out.println("CPF Válido: " + matcher.group());
          }else{
-             System.out.println("Código inválido no matcher 2!");
-             //O SEGUNDO DÁ INVÁLIDO PORQUE USA MATCHES, VERIFICA A STRING INTEIRA
+             System.out.println("CPF Inválido");
          }
     }
 }
