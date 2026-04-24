67091db7fdaee4e352f4c97d4389dea4cdfdeee2
Gabriel Luna <glunamaia@gmail.com>
2026-04-24 17:04:04 -0300

    exercício de fixação da classe Date, criador de lembrete dinâmico de data de vencimento e horário em Tokyo

--- src/Main.java
+++ src/Main.java
@@ -10,58 +10,36 @@ import java.util.regex.Matcher;
 public class Main
 {
 
-    public static String formatDate(String date){
-        Pattern pattern= Pattern.compile("([0-9]{4})-([0-9]{2})-([0-9]{2})");
-        Matcher matcher = pattern.matcher(date.toString());
-        String formatedDate = "";
-        if(matcher.find()){
-            formatedDate = matcher.group(3) + "/" + matcher.group(2) + "/" + matcher.group(1);
-        }
-        return formatedDate;
-    }
      public static void main(String[] args) {
-        // Data e hora em Java
-        // API nova que é o JavaTime
-        // LocalDate, LocalTime, LocalDateTime
-        // .of(ano, mês, dia, hora ou minuto)
-        // .parse() converte
-        // .now() momento exato
-         LocalDate dataCompra = LocalDate.now();
-         LocalDate dataPrimeiraParcela = LocalDate.of(2026, 4, 30).plusDays(5);
-         LocalDate dataSegundaParcela = dataPrimeiraParcela.plusDays(45); //minusDays
+         Pattern pattern = Pattern.compile("[0-9]{2}[/-][0-9]{2}[/-][0-9]{4}");
+         Scanner scan = new Scanner(System.in);
+         System.out.println("Insira o período de antecedência em dias: ");
+         int antecedencia = 0;
+         try{
+             antecedencia = scan.nextInt();
+         }catch (Exception e){
+             System.out.println("Inserção de valor errado " + e.getMessage());
+         }
+         System.out.println("Insira a data de vencimento (dd-mm-aaaa): ");
+         String date = scan.next();
+         Matcher matcher = pattern.matcher(date);
+         System.out.println(antecedencia);
+         if(matcher.matches()){
+             System.out.println("Data de vencimento: " + date + " é válida");
+         }else{
+             System.out.println("Data de vencimento: " + date + " é INválida");
+         }
 
+         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-         System.out.println("Data da compra: "+ formatDate(dataCompra.toString()));
-         System.out.println("Data da primeira parcela: "+ formatDate(dataPrimeiraParcela.toString()));
-         System.out.println("Data da segunda parcela: "+ formatDate(dataSegundaParcela.toString()));
-         System.out.println(dataCompra.isEqual(LocalDate.now()));
-         System.out.println(dataPrimeiraParcela.isEqual(LocalDate.now()));
+         LocalDate formatedDate = LocalDate.parse(date.replace('/', '-'), formatter);
 
-         //ou ao invés de usar regex, podemos usar DateTime Formatter
+         System.out.println("Data do lembrete: " + formatedDate.minusDays(antecedencia).format(formatter));
 
-         DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-         DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("hh:mm:ss");
-         LocalTime time = LocalTime.now();
-
-         System.out.println("Formato da data da compra com DateTimeFormatter: "+ dataCompra.format(formatterDate));
-         System.out.println("Formato da data da compra com DateTimeFormatter: "+ time.format(formatterTime));
-
-         ZonedDateTime dataConclusaoCompra = ZonedDateTime.now();
-         ZonedDateTime newYork = dataConclusaoCompra.withZoneSameInstant(ZoneId.of("America/New_York"));
-
-         System.out.println("Data da conclusão da compra em NY: "+ newYork.format(formatterDate));
-
-         LocalTime horaInicio = LocalTime.now();
-         LocalTime horaFim = horaInicio.plusHours(1).plusMinutes(30);
-
-         Duration periodo = Duration.between(horaInicio, horaFim);
-
-         System.out.println("Periodo da compra com DateTimeFormatter: "+ periodo.toHours() + "h " + periodo.toMinutesPart() + "m");
-
-         Period periodoPgtoVencimento = Period.between(dataPrimeiraParcela, dataSegundaParcela);
-
-         System.out.println(periodoPgtoVencimento.getMonths() + "meses " +  periodoPgtoVencimento.getDays() + "dias");
-
+         ZonedDateTime fuso = ZonedDateTime.now(); // esse aqui por enquanto pega o valor de onde estou
+         ZonedDateTime Tokio = fuso.withZoneSameInstant(ZoneId.of("Asia/Tokyo")); // aqui nós passamos de qual país queremos o horário
+         System.out.println("Data em Tokyo: " + Tokio.format(formatter));
+         System.out.println("Horário em Tokyo: " + Tokio.format(DateTimeFormatter.ofPattern("hh:mm:ss")));
      }
 }
 
