c4faeab211d6c022ab6492d63a8b01b98da6d552
Gabriel Luna <glunamaia@gmail.com>
2026-05-19 23:29:05 -0300

    encapsulamento em POO

--- src/Livro.java
+++ src/Livro.java
@@ -3,10 +3,10 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 public class Livro {
-    String titulo;
-    String  autor;
-    LocalDate data;
-    double valor;
+    private String titulo;
+    private String autor;
+    private LocalDate data;
+    private double valor;
 
     public Livro(String titulo, String autor, LocalDate data, double valor) {
         this.titulo = titulo;
@@ -15,8 +15,28 @@ public class Livro {
         this.valor = valor;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
     public void exibirInfo(){
         String date = this.data.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        System.out.printf("%s - %s. Data de publicação: %s . R$%f\n", this.titulo, this.autor, date, this.valor);
+        System.out.printf("%s - %s. Data de publicação: %s . R$%.2f\n", this.titulo, this.autor, date, this.valor);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,16 @@ public class Main
 
      public static void main(String[] args) {
 
-         List<String> funcionarios = List.of("Ana", "Bruno", "Carlos", "Amanda", "Alice", "Daniel", "Caroline");
-         List<Integer> numeros = List.of(2, 3, 5, 7, 11);
-         List<String> funcionariosComMenosDe5Letras = funcionarios.stream().filter(s -> s.length() <= 5)
-                         .collect(Collectors.toList());
-         List<Integer> quadrados =  numeros.stream().map(valor -> valor * valor).collect(Collectors.toList());
-         List<Double> precosProdutos = List.of(29.99, 49.50, 15.75, 99.99);
-         Double precoTotal = precosProdutos.stream().reduce(0.0, Double::sum);
-         Double precosComImposto =  precoTotal + (precoTotal * 0.08);
-         List<Double> notas = List.of(7.5, 8.0, 6.5, 9.0, 10.0);
-         Double menorNota = notas.stream().min(Double::compareTo).get();
-         Double maiorNota = notas.stream().max(Double::compareTo).get();
-         Double media = notas.stream().reduce(0.0, Double::sum)/notas.size();
-         System.out.println(funcionarios);
-         System.out.println(funcionariosComMenosDe5Letras);
-         System.out.println(numeros);
-         System.out.println(quadrados);
-         System.out.println("Total sem imposto: " + precoTotal);
-         System.out.printf("Total com imposto %.2f: ", precosComImposto);
-         System.out.printf("Menor nota %.2f: ", menorNota);
-         System.out.printf("Maior nota %.2f: ", maiorNota);
-         System.out.printf("Média das notas %.2f: ", media);
+        LocalDate now = LocalDate.now();
+        Livro livro1 = new Livro("Diário de um Banana", "Jeff Kiney", now, 15.90);
+        livro1.exibirInfo();
+        livro1.setValor(15.40);
+        livro1.exibirInfo();
+
+        String date = "10/05/2026";
+        LocalDate data = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+         System.out.println(data);
+
      }
 }
